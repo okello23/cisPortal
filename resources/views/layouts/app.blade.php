6b8bef1efbc17a8af95c42148ded5944ac52ebf8
okello23 <benbyron24@gmail.com>
2026-06-18 15:05:21 +0300

    added logo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>{{ $title ?? 'CPHL ICT Support Portal' }}</title>
+    <link rel="icon" type="image/png" href="{{ asset('coa2.png') }}">
+    <link rel="apple-touch-icon" href="{{ asset('coa2.png') }}">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@300;400;500;600;700&display=swap" rel="stylesheet">
