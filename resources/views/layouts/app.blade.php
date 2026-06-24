d73b06a141e112bb4299ece3da8180d2a1815179
okello23 <benbyron24@gmail.com>
2026-06-24 11:27:22 +0300

    Fix Livewire asset injection

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -139,7 +139,6 @@
             color: #fff !important;
         }
     </style>
-    @livewireStyles
     @stack('styles')
 </head>
 <body>
@@ -184,7 +183,6 @@
     </main>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-    @livewireScripts
     @stack('scripts')
 </body>
 </html>
